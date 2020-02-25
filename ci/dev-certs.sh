f151b1d58b1d4540e837fa1c2853f4da1af2cb38
Davide De Rosa <keeshux@gmail.com>
2020-02-25 19:01:15 +0100

    Force new devices provisioning right in lane

--- ci/dev-certs.sh
+++ ci/dev-certs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-bundle exec fastlane --env secret-codesign,secret-deploy update_provisioning type:adhoc readonly:false force_for_new_devices:true
+bundle exec fastlane --env secret-codesign,secret-deploy update_provisioning type:adhoc readonly:false

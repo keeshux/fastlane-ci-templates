96d5f0c5d5417a097367d7b65b3aeb1294df91b2
Davide De Rosa <keeshux@gmail.com>
2020-04-22 11:54:59 +0200

    Copy en release notes to other l10ns by default

--- ci/commit-changelog.sh
+++ ci/commit-changelog.sh
@@ -4,13 +4,16 @@ BUILD=$((`ci/build-number.sh` + 1))
 DATE=`date "+%Y-%m-%d"`
 CHANGELOG="CHANGELOG.md"
 CHANGELOG_PREFACE="CHANGELOG.preface.md"
-RELEASE_NOTES="fastlane/metadata/en-US/release_notes.txt"
+FASTLANE_METADATA="fastlane/metadata"
+RELEASE_NOTES="$FASTLANE_METADATA/en-US/release_notes.txt"
 
 sed "s/Unreleased/$VERSION Beta $BUILD ($DATE)/" $CHANGELOG >$CHANGELOG.tmp
 mv $CHANGELOG.tmp $CHANGELOG
 ci/latest-changelog.sh | ci/strip-issues.sh >templates/$CHANGELOG.tmp
 cp templates/$CHANGELOG.tmp $RELEASE_NOTES
+ci/copy-release-notes.sh
 cat templates/$CHANGELOG_PREFACE templates/$CHANGELOG.tmp >templates/$CHANGELOG
 rm templates/$CHANGELOG.tmp
-git add $CHANGELOG $RELEASE_NOTES
+
+git add $CHANGELOG $FASTLANE_METADATA
 git commit -m "Set beta release"

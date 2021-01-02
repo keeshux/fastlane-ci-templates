3e23f4938ab858316dfa88ae9145c9916b00c44c
Davide De Rosa <keeshux@gmail.com>
2021-01-02 23:35:32 +0100

    Refactor other scripts to platform

--- ci/commit-changelog.sh
+++ ci/commit-changelog.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+. .env.$1
+
 VERSION=`ci/version-number.sh`
 BUILD=$((`ci/build-number.sh` + 1))
 DATE=`date "+%Y-%m-%d"`
@@ -7,13 +9,13 @@ CHANGELOG_PREFACE="CHANGELOG.preface.md"
 FASTLANE_METADATA="fastlane/metadata"
 RELEASE_NOTES="$FASTLANE_METADATA/en-US/release_notes.txt"
 
-sed "s/Unreleased/$VERSION Beta $BUILD ($DATE)/" $CHANGELOG >$CHANGELOG.tmp
-mv $CHANGELOG.tmp $CHANGELOG
-ci/latest-changelog.sh | ci/strip-issues.sh >templates/$CHANGELOG.tmp
-cp templates/$CHANGELOG.tmp $RELEASE_NOTES
-ci/copy-release-notes.sh
+sed "s/Unreleased/$VERSION Beta $BUILD ($DATE)/" $APP_ROOT/$CHANGELOG >$APP_ROOT/$CHANGELOG.tmp
+mv $APP_ROOT/$CHANGELOG.tmp $APP_ROOT/$CHANGELOG
+ci/latest-changelog.sh $1 | ci/strip-issues.sh >templates/$CHANGELOG.tmp
+cp templates/$CHANGELOG.tmp $APP_ROOT/$RELEASE_NOTES
+ci/copy-release-notes.sh $1
 cat templates/$CHANGELOG_PREFACE templates/$CHANGELOG.tmp >templates/$CHANGELOG
 rm templates/$CHANGELOG.tmp
 
-git add $CHANGELOG $FASTLANE_METADATA
-git commit -m "Set beta release"
+git add $APP_ROOT/$CHANGELOG $APP_ROOT/$FASTLANE_METADATA
+git commit -m "Set beta release ($APP_CONTEXT)"

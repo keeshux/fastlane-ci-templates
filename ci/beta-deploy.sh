88b06fa3cd3f3a525107b02d8d004811f90287d9
Davide De Rosa <keeshux@gmail.com>
2021-01-04 00:14:30 +0100

    Set specific env order
    - Platform
    - Target
    - Target (platform-specific)
    - Secrets

--- ci/beta-deploy.sh
+++ ci/beta-deploy.sh
@@ -2,4 +2,4 @@
 PLATFORM=$1
 VERSION=`agvtool mvers -terse1`
 BUILD=`agvtool vers -terse`
-bundle exec fastlane --env beta,$PLATFORM,"beta.$PLATFORM",secret-deploy $PLATFORM beta_deploy "${@:2}" && git tag "v$VERSION-b$BUILD"
+bundle exec fastlane --env $PLATFORM,beta,"beta.$PLATFORM",secret-deploy $PLATFORM beta_deploy "${@:2}" && git tag "v$VERSION-b$BUILD"
--- ci/dev-deploy.sh
+++ ci/dev-deploy.sh
@@ -2,4 +2,4 @@
 PLATFORM=$1
 VERSION=`agvtool mvers -terse1`
 BUILD=`agvtool vers -terse`
-bundle exec fastlane --env dev,$PLATFORM,"dev.$PLATFORM",secret-deploy $PLATFORM dev_deploy "${@:2}" && git tag "v$VERSION-a$BUILD"
+bundle exec fastlane --env $PLATFORM,dev,"dev.$PLATFORM",secret-deploy $PLATFORM dev_deploy "${@:2}" && git tag "v$VERSION-a$BUILD"

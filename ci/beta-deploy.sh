2abf37faeea34aa5647be1bd21abd0e0663ac0ee
Davide De Rosa <keeshux@gmail.com>
2021-01-30 10:18:53 +0100

    Skip tagging

--- ci/beta-deploy.sh
+++ ci/beta-deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 PLATFORM=$1
-VERSION=`agvtool mvers -terse1`
-BUILD=`agvtool vers -terse`
-bundle exec fastlane --env $PLATFORM,beta,"beta.$PLATFORM",secret-deploy $PLATFORM beta_deploy "${@:2}" && git tag "v$VERSION-b$BUILD"
+bundle exec fastlane --env $PLATFORM,beta,"beta.$PLATFORM",secret-deploy $PLATFORM beta_deploy "${@:2}"
--- ci/dev-deploy.sh
+++ ci/dev-deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 PLATFORM=$1
-VERSION=`agvtool mvers -terse1`
-BUILD=`agvtool vers -terse`
-bundle exec fastlane --env $PLATFORM,dev,"dev.$PLATFORM",secret-deploy $PLATFORM dev_deploy "${@:2}" && git tag "v$VERSION-a$BUILD"
+bundle exec fastlane --env $PLATFORM,dev,"dev.$PLATFORM",secret-deploy $PLATFORM dev_deploy "${@:2}"

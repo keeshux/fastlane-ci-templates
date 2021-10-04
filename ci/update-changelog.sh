e0bfc0d42300df9e06f89ef919884efa3e671e67
Davide De Rosa <keeshux@gmail.com>
2021-10-04 18:39:07 +0200

    Fix changelog generators

--- ci/update-changelog.sh
+++ ci/update-changelog.sh
@@ -9,6 +9,9 @@ CHANGELOG_PLATFORM="CHANGELOG.$1.md"
 CHANGELOG_PREFACE="CHANGELOG.preface.md"
 FASTLANE_METADATA="fastlane/metadata"
 RELEASE_NOTES="$FASTLANE_METADATA/en-US/release_notes.txt"
+STRIPPED_ISSUES_SUB="s/^(.*)\. \[.*$/\1./"
 
 sed "s/Unreleased/$VERSION Beta $BUILD ($DATE)/" $APP_ROOT/$CHANGELOG >$CHANGELOG.tmp
 mv $CHANGELOG.tmp $APP_ROOT/$CHANGELOG
+
+ci/latest-changelog.sh $1 | sed -E "$STRIPPED_ISSUES_SUB" >$APP_ROOT/$RELEASE_NOTES

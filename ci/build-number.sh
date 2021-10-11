a924d497f33b265164614f568fba4414dcedc09d
Davide De Rosa <keeshux@gmail.com>
2021-10-11 19:23:22 +0200

    Read build/version number via APP_ROOT
    Multiplatform rather than Mac only.

--- ci/build-number.sh
+++ ci/build-number.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-. .env.$1
-BASE_NUMBER=`bundle exec fastlane run get_info_plist_value key:CFBundleVersion path:$APP_ROOT/Info.plist 2>/dev/null | grep Result | sed -E "s/^.*Result: ([0-9]+).*$/\\1/"`
-COMMITS_COUNT=`git rev-list --count HEAD`
-echo $((BASE_NUMBER + COMMITS_COUNT))
+bundle exec fastlane --env $1 get_commits_build_number 2>/dev/null | grep "Build: " | sed -E "s/^.*Build: (.*)$/\1/g"
--- ci/version-number.sh
+++ ci/version-number.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-. .env.$1
-bundle exec fastlane run get_info_plist_value key:CFBundleShortVersionString path:$APP_ROOT/Info.plist 2>/dev/null | grep Result | sed -E "s/^.*Result: ([0-9\.].+).*$/\\1/"
+bundle exec fastlane --env $1 get_version_number_unix 2>/dev/null | grep "Version: " | sed -E "s/^.*Version: (.*)$/\1/g"

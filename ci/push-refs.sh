149a7f44475708f5b75889fd489bdbd6a063232b
Davide De Rosa <keeshux@gmail.com>
2021-11-12 11:05:15 +0100

    Fail fast when pushing refs

--- ci/push-refs.sh
+++ ci/push-refs.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-git push && git push github
-git push --tags && git push --tags github
-git checkout stable && git merge master
-git push github
+if !(git checkout master && git push && git push github); then
+    echo "Error while pushing master"
+    exit
+fi
+if !(git push --tags && git push --tags github); then
+    echo "Error while pushing tags"
+    exit
+fi
+if !(git checkout stable && git merge master && git push github); then
+    echo "Error while pushing stable"
+    exit
+fi
 git checkout master

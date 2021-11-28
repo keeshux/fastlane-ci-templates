219ab6fdc95dd7c4bc6e2f67e2fd00421235bc44
Davide De Rosa <keeshux@gmail.com>
2021-11-28 22:50:31 +0100

    Pick current build number, do not increment

--- ci/build-number.sh
+++ ci/build-number.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bundle exec fastlane --env $1 get_commits_build_number 2>/dev/null | grep "Build: " | sed -E "s/^.*Build: (.*)$/\1/g"
+bundle exec fastlane --env $1 get_build_number_unix 2>/dev/null | grep "Build: " | sed -E "s/^.*Build: (.*)$/\1/g"
